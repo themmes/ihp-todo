d802de32da6c17494587705bd633464691935b90
Tom Hemmes <t.hemmes@geophy.com>
2020-08-23 09:19:40 +0200

    Add some more icons

--- Web/View/Todos/Index.hs
+++ Web/View/Todos/Index.hs
@@ -19,7 +19,8 @@ instance View IndexView ViewContext where
   <div>
           {forEach todos renderTodo}
       <div class="bg-gray-50 px-4 py-5 sm:px-6">
-         <a href={pathTo NewTodoAction}><button class="hover:bg-gray-100 rounded-full p-3">
+         <a href={pathTo NewTodoAction}>
+         <button class="hover:bg-gray-200 rounded-full p-3">
            <svg viewBox="0 0 16 16" class="h-6 w-6 fill-current text-gray-800" xmlns="http://www.w3.org/2000/svg">
              <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4a.5.5 0 0 0-1 0v3.5H4a.5.5 0 0 0 0 1h3.5V12a.5.5 0 0 0 1 0V8.5H12a.5.5 0 0 0 0-1H8.5V4z"/>
            </svg>
@@ -38,9 +39,22 @@ renderTodo todo = [hsx|
           {get #description todo}
         </p>
         <div class="space-x-3">
-          <a class="text-sm text-gray-500 hover:text-blue-500" href={ShowTodoAction (get #id todo)}>Show</a>
-          <a class="text-sm text-gray-500 hover:text-blue-500" href={EditTodoAction (get #id todo)}>Edit</a>
-          <a class="text-sm text-gray-500 hover:text-blue-500 js-delete" href={DeleteTodoAction (get #id todo)}>Delete</a>
+          <a href={EditTodoAction (get #id todo)}>
+            <button class="p-3 hover:bg-gray-300 text-gray-500 hover:text-gray-600 rounded-full">
+              <svg class="h-6 w-6 fill-current " viewBox="0 0 16 16"  xmlns="http://www.w3.org/2000/svg">
+                <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456l-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+                <path d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
+              </svg>
+            </button>
+          </a>
+          <a href={DeleteTodoAction (get #id todo)} class="js-delete">
+            <button class="p-3 hover:bg-gray-300 text-gray-500 hover:text-gray-600 rounded-full">
+              <svg class="h-6 w-6 fill-current " viewBox="0 0 16 16"  xmlns="http://www.w3.org/2000/svg">
+                <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+                <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+              </svg>
+            </button>
+          </a>
       </div>
       </div>
 |]
